3d406ed620cf41adcdac0059adfe0d501bcc9e75
Olga <olgahrytsiuk777@gmail.com>
2025-01-28 00:16:29 +0100

    create model Book in models.py

--- books_service/models.py
+++ books_service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOISE = [
+        ('HC', 'HARD'),
+        ('SC', 'SOFT')
+    ]
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=255, choices=COVER_CHOISE)
+    inventory = models.PositiveIntegerField(default=0)
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2, default=0)
