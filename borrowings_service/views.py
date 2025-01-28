b494c454b09d5593a1638f1d17535e4e792ab946
Olga <olgahrytsiuk777@gmail.com>
2025-01-28 18:20:51 +0100

    create views.py for borrowings_service

--- borrowings_service/views.py
+++ borrowings_service/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowings_service.models import Borrowing
+from borrowings_service.serializers import BorrowingSerializer, BorrowingListSerializer
+
+
+class BorrowingsViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.select_related()
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return BorrowingListSerializer
+        return BorrowingSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action == 'list':
+            return queryset.select_related()
+        return queryset
