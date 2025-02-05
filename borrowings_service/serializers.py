5294d9b800aad7b71174d9ae0a363209612f4040
Olga <olgahrytsiuk777@gmail.com>
2025-02-05 19:46:13 +0100

    create PaymentSerializer

--- borrowings_service/serializers.py
+++ borrowings_service/serializers.py
@@ -1,14 +1,30 @@
 from rest_framework import serializers
 
 from books_service.serializers import BookSerializer
-from borrowings_service.models import Borrowing
+from borrowings_service.models import Borrowing, Payment
+from users_service.serializers import UserSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
-        fields = ("id", "borrowing_data", "expected_date", 'actual_return', 'book_id', 'user_id')
+        fields = "__all__"
 
 
 class BorrowingListSerializer(BorrowingSerializer):
-    book_id = BookSerializer()
+    # book_id = BookSerializer(read_only=True)
+    # user_id = UserSerializer(read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Borrowing
+        fields = "__all__"
+
+    def get_total_price(self, obj):
+        return obj.total_price
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
