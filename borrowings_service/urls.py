9baae9f94587858a449a52ca42f48597f2cbe0ae
Olga <olgahrytsiuk777@gmail.com>
2025-02-05 19:47:19 +0100

    add router for PaymentViewSet, SuccessView, CancelView, webhook

--- borrowings_service/urls.py
+++ borrowings_service/urls.py
@@ -1,13 +1,18 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from books_service.views import BookViewSet
-from borrowings_service.views import BorrowingsViewSet
+from borrowings_service import views
+from borrowings_service.views import BorrowingsViewSet, PaymentViewSet, SuccessView, CancelView, my_webhook_view
 
 router = routers.DefaultRouter()
 router.register("borrowings", BorrowingsViewSet)
+router.register("payment", PaymentViewSet)
 urlpatterns = [
     path("", include(router.urls)),
+    path("success/", SuccessView.as_view(), name="success"),
+    path("cancel/", CancelView.as_view(), name="cancel"),
+    path("webhook/", my_webhook_view, name="webhook"),
 ]
+
 
 app_name = "borrowings_service"
