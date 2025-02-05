c379bada83b5f13137d4a29470d7dd20f98f44c2
Olga <olgahrytsiuk777@gmail.com>
2025-02-05 19:48:56 +0100

    add permissions.py

---
+++ borrowings_service/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsAdminAllORIsAuthenticatedReadOnly(BasePermission):
+    """
+    The request is authenticated as an admin - read/write, if as a user read only request.
+    """
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS
+            and request.user
+            and request.user.is_authenticated
+        ) or (
+            request.user and request.user.is_staff
+        )
+
